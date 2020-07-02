7ca8b039a171fcad29913d9e666b8ddd62b3e3e8
Kingsleymuturi <kingsleymuturi9@gmail.com>
2020-07-02 18:24:19 +0300

    deployment to heroku

--- app/request.py
+++ app/request.py
@@ -87,6 +87,5 @@ def search_movie(movie_name):
             search_movie_list = search_movie_response['results']
             search_movie_results = process_results(search_movie_list)
 
-
     return search_movie_results
 
