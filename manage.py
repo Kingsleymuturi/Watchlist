2dfb227f0ed3bc4bbd75343c3d201e894907e195
Kingsleymuturi <kingsleymuturi9@gmail.com>
2020-07-03 17:17:00 +0300

    update

--- manage.py
+++ manage.py
@@ -1,8 +1,15 @@
 from app import create_app
+from app.models import Movie, Review
 from waitress import serve
 
 # Creating app instance
 app = create_app('development')
+
+@app.shell_context_processor
+def make_shell_context():
+    """Makes the shell context, allowing for specific variables, and classes
+       to be known by the interactive Python session on startup."""
+    return {'Movie': Movie, 'Review': Review}
 
 
 if __name__ == '__main__':
